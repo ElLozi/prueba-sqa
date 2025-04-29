d42252589de927e0900f341a3e9842d642f30741
Luis Mazo <DavidLozanom07@gmail.com>
2025-04-29 16:06:50 -0500

    Actualización del archivo ejecucion.spec.ts

--- tests/ejecucion.spec.ts
+++ tests/ejecucion.spec.ts
@@ -4,14 +4,14 @@ import { ProductPage } from '../pages/pagina.producto';
 import { CartPage } from '../pages/pagina.carrito';
 
 test('Prueba técnica automatización ', async ({ page }) => {
-  test.setTimeout(60000);
+  test.setTimeout(70000);
   const homePage = new HomePage(page);
   const productPage = new ProductPage(page);
   const cartPage = new CartPage(page);
 
   await page.goto('https://sanangel.com.co/');
 
-  // Producto 1: Versalles
+  // Producto 1
   await homePage.clickProduct('Baldado de amor');
   await productPage.setQuantity(2);
   await productPage.addToCartAndGoToCheckout();
@@ -19,7 +19,7 @@ test('Prueba técnica automatización ', async ({ page }) => {
   // Volver a home para segundo producto
   await page.goto('https://sanangel.com.co/');
 
-  // Producto 2: Presente
+  // Producto 2
   await homePage.clickProduct('Presente');
   await productPage.setQuantity(5);
   await productPage.addToCartAndGoToCheckout();
